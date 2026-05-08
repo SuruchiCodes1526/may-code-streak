21eb52a2b863dd72e8a7f9d404eb91ab15047625
Suruchi Kumari <201318258+SuruchiCodes1526@users.noreply.github.com>
2026-05-08 10:04:37 +0530

    Add Day 8 array sum program

---
+++ day8.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    int arr[5], i, sum = 0;
+
+    printf("Enter 5 numbers:
+");
+    for (i = 0; i < 5; i++) {
+        scanf("%d", &arr[i]);
+        sum += arr[i];
+    }
+
+    printf("Sum = %d
+", sum);
+
+    return 0;
+}
